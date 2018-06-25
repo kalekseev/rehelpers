19877aaa633024265ed0ba0fd92810dda6abf358
Konstantin Alekseev <kv.alekseev@gmail.com>
2018-06-25 18:33:09 +0300

    Update redux utils

--- reduxUtils.ts
+++ reduxUtils.ts
@@ -6,8 +6,21 @@ export type ActionsUnion<A extends ActionCreatorsMapObject> = ReturnType<A[keyof
 interface ActionWithPayload<T extends string, P> extends Action<T> {
   payload: P
 }
+interface ActionWithPayloadMeta<T extends string, P, M> extends Action<T> {
+  payload: P,
+  meta: M,
+}
 export function createAction<T extends string>(type: T): Action<T>
 export function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>
-export function createAction<T extends string, P>(type: T, payload?: P) {
-  return payload === undefined ? { type } : { type, payload }
+export function createAction<T extends string, P, M>(type: T, payload: P, meta: M): ActionWithPayloadMeta<T, P, M>
+export function createAction<T extends string, P, M>(type: T, payload?: P, meta?: M) {
+  if (payload === undefined && meta === undefined) {
+    return {type};
+  } else if (payload && meta === undefined) {
+    return { type, payload }
+  } else if (meta && payload === undefined) {
+    return { type, meta }
+  } else {
+    return { type, payload, meta }
+  }
 }
